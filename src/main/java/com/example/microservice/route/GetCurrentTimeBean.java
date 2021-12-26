6937c134941cddc3a4c4cd580dc0b61f51f66e62
Rohit Sharma <rohitsharma8650@gmail.com>
2021-12-26 18:15:07 +0530

    added comment intentionally  to show some changes in a file

--- src/main/java/com/example/microservice/route/GetCurrentTimeBean.java
+++ src/main/java/com/example/microservice/route/GetCurrentTimeBean.java
@@ -8,5 +8,6 @@ import java.time.LocalDateTime;
 public class GetCurrentTimeBean {
     public String getCurrentTime(){
         return "Time Now is "+ LocalDateTime.now();
+        // making this comment intentionaly to have some changes in this file
     }
 }
